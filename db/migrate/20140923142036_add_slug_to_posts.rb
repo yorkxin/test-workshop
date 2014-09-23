330901e45f7b6b0c82d33945afcdae12321d8e9f
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-09-23 22:21:36 +0800

    add slug to posts

---
+++ db/migrate/20140923142036_add_slug_to_posts.rb
@@ -0,0 +1,7 @@
+class AddSlugToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :slug, :string
+
+    add_index :posts, :slug
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923140855) do
+ActiveRecord::Schema.define(version: 20140923142036) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20140923140855) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "state"
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug"
 
 end
