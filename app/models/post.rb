c5690cc54758037b753732c4d1ff5c9336d79ba8
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-09-23 22:30:26 +0800

    make it a simple blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  before_create :generate_slug
+
+  # Prettify URL
+  def to_param
+    "#{self.id}-#{self.slug}"
+  end
+
+  private
+  def generate_slug
+    self.slug = self.title.gsub(/[^A-Za-z0-9]+/, '-').downcase
+  end
 end
