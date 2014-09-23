e1675ba7153d6e940739144bb931fcb22a764900
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-09-23 22:09:25 +0800

    add state field to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923140426) do
+ActiveRecord::Schema.define(version: 20140923140855) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state"
   end
 
 end
