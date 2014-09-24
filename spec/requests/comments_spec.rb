07ea9c67473dd5c6136f03c244680f36bfaa331f
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-09-24 20:23:24 +0800

    example of request spec

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,8 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "Comments", :type => :request do
   describe "POST /posts/xxx/comments" do
-    it "works! (now write some real specs)" do
-      pending "add some tests here"
+    let!(:the_post) {
+      Post.create(:title => "dont care", :body => "dont care")
+    }
+
+    subject {
+      post "/posts/1/comments", :comment => comment_params
+    }
+
+    context "filling all fields" do
+      let(:comment_params) {
+        {
+          :author => "John Appleseed",
+          :content => "Good article. I like it."
+        }
+      }
+
+      context "a post that is published" do
+        before {
+          the_post.publish!
+        }
+
+        it "creates a new comment" do
+          expect {
+            subject
+          }.to change { the_post.comments.count }.by(1)
+        end
+      end
+
+      context "a post that is draft" do
+        it "does not create a new comment" do
+          expect {
+            subject rescue nil # ignore any exception
+          }.not_to change { the_post.comments.count }
+        end
+      end
     end
+
   end
 end
