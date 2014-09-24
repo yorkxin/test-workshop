7cbd820c3e5d8e0ff82180c43171da5b9db7bb08
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-09-24 19:54:45 +0800

    add request spec for comments controller

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", :type => :request do
+  describe "POST /posts/xxx/comments" do
+    it "works! (now write some real specs)" do
+      pending "add some tests here"
+    end
+  end
+end
